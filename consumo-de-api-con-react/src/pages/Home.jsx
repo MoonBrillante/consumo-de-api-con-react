7dcb8deda4e63f352984261aa555dd2911b26f8b
MoonBrillante <32852571+MoonBrillante@users.noreply.github.com>
2023-07-10 06:58:23 +0200

    Update Home.jsx

--- consumo-de-api-con-react/src/pages/Home.jsx
+++ consumo-de-api-con-react/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Grid } from "@mui/material";
-import { Container } from "@mui/system";
+import {Box, Container } from "@mui/system";
 import axios from "axios";
-import  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import PokemonCard from "../components/PokemonCard";
 
